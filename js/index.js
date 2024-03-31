09a838848c9588ad783565e856c734c0701b4fbd
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-03-31 12:33:58 +0500

    solution first example

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// 1-Misol
+// 1 dan 100 gacha bo'lgan sonlarni faqat tublarini chiqarish
+
+// function isPrime(num) {
+//     if (num <= 1) return false;
+//     if (num <= 3) return true;
+
+//     if (num % 2 === 0 || num % 3 === 0) return false;
+
+//     let i = 5;
+//     while (i * i <= num) {
+//         if (num % i === 0 || num % (i + 2) === 0) return false;
+//         i += 6;
+//     }
+//     return true;
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (isPrime(i)) {
+//         console.log(i);
+//     }
+// }
+
+
